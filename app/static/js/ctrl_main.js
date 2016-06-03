e19a249dfd10f35ce9890aeb0f5e5055e147ab23
Diver Dune <sysdeep@yandex.ru>
2016-06-03 15:02:46 +0300

    add main page

--- app/static/js/ctrl_main.js
+++ app/static/js/ctrl_main.js
@@ -8,83 +8,90 @@
 	app.controller("MainCtrl", function($scope, $location, svcData){
 		$scope.data = svcData.data;
 		$scope.data.current_view = "main";
-
-		if(svcData.data.songs_need_update)
-			svcData.get_songs();
-
-
-
-		$scope.refresh = function(){
-			svcData.get_songs();
-		}
+		$scope.singer_songs = [];
+		var svc = svcData;
+		svc.data.singer = {};
 
 
-		$scope.show_song = function(song){
-			console.log(song);
-			// svcData.data.current_song = song;
-			$location.path( "/song/"+song._id );
-		}
 
-		$scope.edit_song = function(song){
-			// svcData.data.song = song;
-			$location.path( "/edit/"+song._id );
-		}
-
-		$scope.remove_song = function(song){
-			svcData.data.song = song;
-			// $location.path( "/song/"+song._id );
-			svcData.remove_song();
-			// $location.path( "/main/" );
-
-		}
-
-		$scope.add = function(){
-			// svcData.set_default_song();
-			$location.path( "/edit/0" );
+		$scope.show_songs = function(singer){
+			svc.data.singer = singer;
+			$scope.singer_songs = svc.data.songs.filter(function(song){return song.singer == singer.id});
 		}
 
 
 
-		$scope.songs_filtered = function(){
+		// $scope.refresh = function(){
+		// 	svcData.get_songs();
+		// }
 
-			var o_data = [];
+
+		// $scope.show_song = function(song){
+		// 	console.log(song);
+		// 	// svcData.data.current_song = song;
+		// 	$location.path( "/song/"+song._id );
+		// }
+
+		// $scope.edit_song = function(song){
+		// 	// svcData.data.song = song;
+		// 	$location.path( "/edit/"+song._id );
+		// }
+
+		// $scope.remove_song = function(song){
+		// 	svcData.data.song = song;
+		// 	// $location.path( "/song/"+song._id );
+		// 	svcData.remove_song();
+		// 	// $location.path( "/main/" );
+
+		// }
+
+		// $scope.add = function(){
+		// 	// svcData.set_default_song();
+		// 	$location.path( "/edit/0" );
+		// }
+
+
+
+		// $scope.songs_filtered = function(){
+
+		// 	var o_data = [];
 			
 
-			angular.forEach(svcData.data.songs, function(song){
-				var add_flag = true;
+		// 	angular.forEach(svcData.data.songs, function(song){
+		// 		var add_flag = true;
 
-				//-- search
-				if(svcData.data.filter.search.singer.length > 0){
-					var s_s = song.singer.toLowerCase();
-					var d_s = svcData.data.filter.search.singer.toLowerCase();
-					if(s_s.indexOf(d_s) == -1)
-						add_flag = false;
-				}
-
-
-				if(add_flag){
-					if(svcData.data.filter.search.quick.length > 0){
-						var s_singer = song.singer.toLowerCase();
-						var s_author = song.author.toLowerCase();
-						var s_name = song.name.toLowerCase();
-						var d_s = svcData.data.filter.search.quick.toLowerCase();
-						if((s_singer.indexOf(d_s) == -1)&&(s_author.indexOf(d_s) == -1)&&(s_name.indexOf(d_s) == -1)){
-							add_flag = false;
-						}
-					}
-				}
+		// 		//-- search
+		// 		if(svcData.data.filter.search.singer.length > 0){
+		// 			var s_s = song.singer.toLowerCase();
+		// 			var d_s = svcData.data.filter.search.singer.toLowerCase();
+		// 			if(s_s.indexOf(d_s) == -1)
+		// 				add_flag = false;
+		// 		}
 
 
-
-				if(add_flag)
-					o_data.push(song);
-			});
+		// 		if(add_flag){
+		// 			if(svcData.data.filter.search.quick.length > 0){
+		// 				var s_singer = song.singer.toLowerCase();
+		// 				var s_author = song.author.toLowerCase();
+		// 				var s_name = song.name.toLowerCase();
+		// 				var d_s = svcData.data.filter.search.quick.toLowerCase();
+		// 				if((s_singer.indexOf(d_s) == -1)&&(s_author.indexOf(d_s) == -1)&&(s_name.indexOf(d_s) == -1)){
+		// 					add_flag = false;
+		// 				}
+		// 			}
+		// 		}
 
 
 
-			return o_data;
+		// 		if(add_flag)
+		// 			o_data.push(song);
+		// 	});
 
-		}
+
+
+		// 	return o_data;
+
+		// }
 
 
 
