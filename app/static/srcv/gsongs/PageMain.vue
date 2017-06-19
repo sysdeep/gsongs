3933b1bff01e14752c78708a6570ecb30dbd0662
diver nika <sysdeep@yandex.ru>
2017-06-19 12:21:44 +0300

    complete main page

--- app/static/srcv/gsongs/PageMain.vue
+++ app/static/srcv/gsongs/PageMain.vue
@@ -1,72 +1,72 @@
 <template>
-    <div>
-    
-        
-<div class="row">
-	<div class="col-md-3">
-		<h4 class="page-header">
-			Исполнители
-			<span class="pull-right badge">{{ state.singers.length }}</span>
-		</h4>
-
-		<input type="text" class="form-control" v-model="singer_filter" placeholder="быстрый поиск">
-		
-		<br>
-
-		<ul class="list-unstyled">
-			<!-- <li v-for="singer in state.singers | filterBy singer_filter"> -->
-			<li v-for="singer in state.singers">
-				<a href="javascript: void(0)" ng-click="self.show_songs(singer)" title="отобразить список песенок для этого исполнителя">
-					{{ singer.name }}
-					
-					<i ng-if="singer.id == self.singer.id" class="fa fa-arrow-left" aria-hidden="true"></i>
-				</a>
-			</li>
-		</ul>
+	<div>
+	
+		<div class="row">
+			<div class="col-md-3">
+				<h4 class="page-header">
+					Исполнители
+					<span class="pull-right badge">{{ state.singers.length }}</span>
+				</h4>
+	
+				<input type="text" class="form-control" v-model="singer_filter" placeholder="быстрый поиск">
+	
+				<br>
+	
+				<ul class="list-unstyled">
+					<li v-for="singer in filtered_singers">
+						<a href="javascript: void(0)" @click="show_songs(singer)" title="отобразить список песенок для этого исполнителя">
+							{{ singer.name }}
+	
+							<i v-if="singer == current_singer" class="fa fa-arrow-left" aria-hidden="true"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+	
+			<div class="col-md-5">
+				<h4 class="page-header">
+					Песни исполнителя:
+	
+					<span v-if="!current_singer">нет исполнителя</span>
+					<span v-if="current_singer">
+						<router-link :to="/singer/ + current_singer.id" title="перейти на страницу исполнителя">
+							{{ current_singer.name }}
+						</router-link>
+						<span class="pull-right badge">{{ singer_songs.length }}</span>
+					</span>
+	
+				</h4>
+	
+				<input type="text" class="form-control" v-model="song_filter" placeholder="быстрый поиск">
+	
+				<br>
+	
+				<ul class="list-unstyled">
+					<li v-for="song in filtered_songs">
+						<router-link :to="/song/ + song.id" title="перейти к странице песни">
+							{{ song.name }}
+						</router-link>
+					</li>
+				</ul>
+	
+			</div>
+	
+			<div class="col-md-4">
+				<h4 class="page-header">
+					Последние добавленные
+					<span class="pull-right badge">{{ last_songs.length }}</span>
+				</h4>
+				<ul>
+					<li v-for="song in last_songs">
+						<router-link :to="/singer/ + song.singer" title="перейти к странице исполнителя">{{ get_singer_name(song.singer) }}</router-link>
+						-
+						<router-link :to="/song/ + song.id" title="перейти к странице песни">{{ song.name }}</router-link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	
 	</div>
-
-
-
-
-	<div class="col-md-5">
-		<h4 class="page-header">
-			Песни исполнителя: 
-			<a href="#/singer/[[ self.singer.id ]]" title="перейти на страницу исполнителя">
-				[[ self.singer.name ]]
-			</a>
-			<span class="pull-right badge">[[ self.singer_songs.length ]]</span>
-		</h4>
-
-		<input type="text" class="form-control" ng-model="song_filter.name" placeholder="быстрый поиск">
-		
-		<br>
-			
-		<ul class="list-unstyled">
-			<li ng-repeat="song in self.singer_songs | filter:song_filter">
-				<a href="#/song/[[ song.id ]]" title="перейти к странице песни">
-					[[ song.name ]]
-				</a>
-			</li>
-		</ul>
-
-	</div>
-
-	<div class="col-md-4">
-		<h4 class="page-header">
-			Последние добавленные
-			<span class="pull-right badge">[[ self.last_songs.length ]]</span>
-		</h4>
-		<ul>
-			<li ng-repeat="song in self.last_songs">
-				<a href="#/singer/[[ song.singer ]]" title="перейти к странице исполнителя">[[ self.get_singer_name(song.singer) ]]</a>
-				-
-				<a href="#/song/[[ song.id ]]" title="перейти к странице песни">[[ song.name ]]</a>
-			</li>
-		</ul>
-	</div>
-</div>
-    
-    </div>
 </template>
 
 
@@ -75,51 +75,74 @@ import storage from "./storage";
 import Q from "q";
 
 
-function delay(){
-    console.log("delay");
-    let deferred = Q.defer();
+function delay() {
+	console.log("delay");
+	let deferred = Q.defer();
 
-    setTimeout(deferred.resolve, 1000);
-    return deferred.promise;
+	setTimeout(deferred.resolve, 1000);
+	return deferred.promise;
 }
 
 
 export default {
-    data: function(){
-        return {
-            "state": storage.state,
+	data: function () {
+		return {
+			"state"				: storage.state,
 
-            "singer_filter"     : ""
-        }
-    },
+			"singer_filter"		: "",
+			"song_filter"		: "",
 
-    created: function(){
+			"current_singer"	: null,
+			"singer_songs"		: [],
+
+			"last_songs"		: [],
+			"last_songs_count"	: 10
+		}
+	},
+
+	created: function () {
+
+		storage.need_singers()
+			.then(storage.need_songs)
+			.then(() => {
+				this.state.songs.sort(function (a, b) { return b.id - a.id });
+				this.last_songs = this.state.songs.slice(0, this.last_songs_count);
+			})
 
 
-        storage.need_singers()
-            .then(storage.need_songs)
-            .then(()=>{
-                console.log("after all");
-            })
+	},
 
-        // delay()
-        //     .then(delay)
-        //     .then(delay)
-        //     .then(delay)
-        //     .then(()=>{
-        //     console.log("after delay");
-        // })
+	methods: {
+		get_singer_name: function (singer_id) {
+			let singer = this.state.singers.find((item) => { return item.id == singer_id })
+			return singer ? singer.name : "---"
+		},
 
-        // storage.need_songs()
-        //     .then(storage.need_songs)
-        //     .then(storage.need_singers())
-        //     .then(()=>{
-        //         console.log("fin")
-        //     })
-        // // storage.need_singers().then(storage.need_songs).then(()=>{
 
-        // // })
-    }
+		show_songs: function (singer) {
+			this.current_singer = singer;
+			this.singer_songs = this.state.songs.filter((item) => { return item.singer == singer.id });
+		}
+	},
+
+
+	computed: {
+		filtered_singers: function () {
+			let lfind = this.singer_filter.toLowerCase();
+			return this.state.singers.filter((item) => {
+				let name = item.name.toLowerCase();
+				return name.indexOf(lfind) > -1;
+			});
+		},
+
+		filtered_songs: function () {
+			let lfind = this.song_filter.toLowerCase();
+			return this.singer_songs.filter((item) => {
+				let name = item.name.toLowerCase();
+				return name.indexOf(lfind) > -1;
+			});
+		}
+	}
 
 
 }
--- app/static/srcv/gsongs/storage.js
+++ app/static/srcv/gsongs/storage.js
@@ -1,88 +1,174 @@
 import net from "./net";
 import Q from "q";
 
-const storage = {
-    "state": {
-        "singers": [],
-        "singers_loaded": false,
+var state = {
+    "singers": [],
+    "singers_loaded": false,
 
-        "songs": [],
-        "songs_loaded": false,
-    },
-
-
-    fetch_singers: function () {
-        console.log("fetch singers");
-        var defer = Q.defer();
-        
-        this.state.singers_loaded = false;
-        net.get_singers().then(response => {
-            this.state.singers = response.data.singers;
-            this.state.singers_loaded = true;
-
-            defer.resolve();
-        })
-
-        return defer.promise;
-    },
+    "songs": [],
+    "songs_loaded": false,
+}
 
 
-    fetch_songs: function () {
-        console.log("fetch songs")
-        var defer = Q.defer();
-
-        this.state.songs_loaded = false;
-        net.get_songs().then(response => {
-            this.state.songs = response.data.songs;
-            this.state.songs_loaded = true;
-
-            defer.resolve();
-        })
-
-        return defer.promise;
-    },
-
-
-
-    need_singers: function(){
-        console.log("need singers");
-        var defer = Q.defer();
-
-        if(this.state.singers_loaded){
-            defer.resolve();
-            return defer.promise;    
-        }else{
-            return this.fetch_singers();
-        }
-
-        
-    },
-
-
-    need_songs: function(){
-        console.log("need songs");
-        console.log(this);
-        let defer = Q.defer();
-
-
-        if(this.state.songs_loaded){
-            defer.resolve();
-            return defer.promise;    
-        }else{
-
-            console.log("123");
-            return this.fetch_songs();
-        }
-
-        
-    },
-
-
-
+export default {
+    "state": state,
+    "fetch_singers": fetch_singers,
+    "fetch_songs": fetch_songs,
+    "need_singers": need_singers,
+    "need_songs": need_songs
 }
 
 
 
 
+function fetch_singers() {
+    console.log("fetch singers");
+    var defer = Q.defer();
 
-export default storage;
+    state.singers_loaded = false;
+    net.get_singers().then(response => {
+        state.singers = response.data.singers;
+        state.singers_loaded = true;
+
+        defer.resolve();
+    })
+
+    return defer.promise;
+}
+
+
+function fetch_songs() {
+    console.log("fetch songs")
+    var defer = Q.defer();
+
+    state.songs_loaded = false;
+    net.get_songs().then(response => {
+        state.songs = response.data.songs;
+        state.songs_loaded = true;
+
+        defer.resolve();
+    })
+
+    return defer.promise;
+}
+
+
+
+function need_singers() {
+    console.log("need singers");
+    var defer = Q.defer();
+
+    if (state.singers_loaded) {
+        defer.resolve();
+        return defer.promise;
+    } else {
+        return fetch_singers();
+    }
+
+
+}
+
+
+function need_songs() {
+    console.log("need songs");
+    // console.log(this);
+    let defer = Q.defer();
+
+
+    if (state.songs_loaded) {
+        defer.resolve();
+        return defer.promise;
+    } else {
+
+        console.log("123");
+        return fetch_songs();
+    }
+}
+
+
+
+// const storage = {
+//     "state": {
+//         "singers": [],
+//         "singers_loaded": false,
+
+//         "songs": [],
+//         "songs_loaded": false,
+//     },
+
+
+//     fetch_singers: function () {
+//         console.log("fetch singers");
+//         var defer = Q.defer();
+
+//         this.state.singers_loaded = false;
+//         net.get_singers().then(response => {
+//             this.state.singers = response.data.singers;
+//             this.state.singers_loaded = true;
+
+//             defer.resolve();
+//         })
+
+//         return defer.promise;
+//     },
+
+
+//     fetch_songs: function () {
+//         console.log("fetch songs")
+//         var defer = Q.defer();
+
+//         this.state.songs_loaded = false;
+//         net.get_songs().then(response => {
+//             this.state.songs = response.data.songs;
+//             this.state.songs_loaded = true;
+
+//             defer.resolve();
+//         })
+
+//         return defer.promise;
+//     },
+
+
+
+//     need_singers: function(){
+//         console.log("need singers");
+//         var defer = Q.defer();
+
+//         if(this.state.singers_loaded){
+//             defer.resolve();
+//             return defer.promise;    
+//         }else{
+//             return this.fetch_singers();
+//         }
+
+
+//     },
+
+
+//     need_songs: function(){
+//         console.log("need songs");
+//         console.log(this);
+//         let defer = Q.defer();
+
+
+//         if(this.state.songs_loaded){
+//             defer.resolve();
+//             return defer.promise;    
+//         }else{
+
+//             console.log("123");
+//             return this.fetch_songs();
+//         }
+
+
+//     },
+
+
+
+// }
+
+
+
+
+
+// export default storage;
