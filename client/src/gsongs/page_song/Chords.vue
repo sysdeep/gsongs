e4b11e8f8727ffd4bc8f9f239f7f53d20ab3db4d
Diver Dune <sysdeep@yandex.ru>
2020-12-03 14:57:13 +0300

    song page - show chords list in song

--- client/src/gsongs/page_song/Chords.vue
+++ client/src/gsongs/page_song/Chords.vue
@@ -19,7 +19,8 @@ export default {
 
     computed: {
         chords_list(){
-            let list = this.text.match(/\[\[(.*?)\]\]/g);
+            // let list = this.text.match(/\[\[(.*?)\]\]/g);
+            let list = this.text.match(/([A-G](#|b)?)(\(?(M|maj|major|m|min|minor|dim|sus|dom|aug)?(\+|-|add)?\d*\)?)(\/([A-G](#|b)?))?/g);
             if(list === null)   return [];
             let strip_list = list.map(line => line.replace(/\[\[/g, "").replace(/\]\]/g, "").replace(/\s/g, ''))
 
