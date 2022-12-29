1f240525a255fef2e8be153f30f0ed3eb5c7704a
Igor Nefedkin <igor.nefedkin@kaspersky.com>
2022-12-29 15:53:31 +0300

    update todo app

--- client/wp.todo.config.js
+++ client/wp.todo.config.js
@@ -1,134 +1,51 @@
 var path = require("path");
-// var webpack = require('webpack');
-const { VueLoaderPlugin } = require('vue-loader');
-
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
-// var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-
+const { VueLoaderPlugin } = require("vue-loader");
 
 var config = {
 	entry: {
-
-		// "gsongs"		: path.join(__dirname, "app", "srcv", "gsongs", "app.js"),
-		"todo"			: path.join(__dirname, "src", "todo", "main.js"),
-		// "chords_edit"	: path.join(__dirname, "app", "srcv", "chords_edit", "main.js"),
-		// "chords_view"	: path.join(__dirname, "app", "srcv", "chords_view", "main.js"),
-		
-		// "vendor"	: ["vue", "axios"]
-		
+		todo: path.join(__dirname, "src", "todo", "main.js"),
 	},
 	output: {
 		path: path.join(__dirname, "..", "app", "static", "build"),
-		// path: __dirname + 'app/static/build',
-		filename: '[name].js',
-		// filename: 'templates.js',
+		filename: "[name].js",
 	},
 	resolve: {
-		// extensions: ['.js'],
-		// alias: { vue: 'vue/dist/vue.js' }
-		alias: { 'vue$': 'vue/dist/vue.esm.js' }
+		alias: {
+			// vue$: "vue/dist/vue.esm.js"
+			"@": path.resolve(path.join(__dirname, "src", "todo")),
+		},
 	},
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-				loader: 'babel-loader',
+				loader: "babel-loader",
 				exclude: /node_modules/,
-				// query: {
-				// 	presets: ['es2015']
-				// }
 			},
-			// {
-			// 	test: /\.(css|html)$/,
-			// 	loader: 'raw-loader'
-			// },
-
 			{
 				test: /\.css$/,
-				use: [
-					'vue-style-loader',
-					'css-loader'
-				]
+				use: ["vue-style-loader", "css-loader"],
 			},
-
-
-			// {
-			// 	test: /\.(png|jpg|gif|svg)$/,
-			// 	loader: 'file-loader',
-			// 	options: {
-			// 		name: '[name].[ext]?[hash]'
-			// 	}
-			// },
 
 			{
 				test: /\.vue$/,
-				loader: 'vue-loader',
-				// query: {
-				//     presets: ['es2015']
-				// },
-				// options: {
-				// 	loaders: {
-				// 		// js: 'babel-loader?presets[]=es2015'
-				// 	}
-				// 	// other vue-loader options go here
-				// }
-			}
-			
-		]
+				loader: "vue-loader",
+			},
+		],
 	},
-	// plugins: [
-	//   new webpack.optimize.UglifyJsPlugin(),
-	// ]
-	// plugins : [
-	// 	new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.js' })
-	// ]
-	plugins : [
-		new VueLoaderPlugin(),
-	],
-
-
-	// //--- new cunks api
-	// optimization: {
-	// 	splitChunks: {
-	// 		// chunks: "all"
-	// 		cacheGroups: {
-	// 			mdispatch_vendor: {
-	// 				name: 'gsongs_vendor',
-    //       			chunks: 'initial',
-    //       			minChunks: 2,
-	// 				// filename: '[name].bundle.js'
-	// 			},
-
-	// 			// mdispatch_store: {
-	// 			// 	name: 'mdispatch_store',
-    // //       			chunks: 'initial',
-    // //       			minChunks: 2,
-	// 			// 	// filename: '[name].bundle.js'
-	// 			// }
-	// 		}
-	// 	}
-	// }
-
-
+	plugins: [new VueLoaderPlugin()],
 };
 
-
-
-if(process.env.hasOwnProperty("PRODUCTION")){
+if (process.env.hasOwnProperty("PRODUCTION")) {
 	console.log("Start build app");
 	config.watch = false;
-	config.devtool = 'source-map';
-	// config.plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: config.devtool}));
-	// config.stats = "normal";
+	config.devtool = "source-map";
 	config.mode = "production";
-}else{
+} else {
 	console.log("Start watch app");
 	config.watch = true;
-	config.devtool = 'inline-source-map';
+	config.devtool = "inline-source-map";
 	config.mode = "development";
 }
-
-
 
 module.exports = config;
